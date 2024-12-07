32df7195615f4f82ac254dafc28da53c1d4205d5
Shared Projects <altushost@smokerscircle.io>
2024-12-07 01:09:59 +0100

    About3 page added

--- src/components/RenderMarkdown.js
+++ src/components/RenderMarkdown.js
@@ -3,10 +3,17 @@ import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkBreaks from "remark-breaks";
 
+function removeNewlines(input) {
+  return input.replace(/\n/g, "");
+}
+
 const RenderMarkdown = ({ content }) => {
+
+  const result = removeNewlines(content);
+  
   return (
     <ReactMarkdown
-      children={content}
+      children={result}
       rehypePlugins={[rehypeRaw]} // Allows raw HTML to be rendered
       remarkPlugins={[remarkBreaks]} // Converts \n to <br />
     />
