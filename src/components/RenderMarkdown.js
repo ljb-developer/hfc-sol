dcd4de09a648b090437e89e4224bae6faa954dc4
Shared Projects <altushost@smokerscircle.io>
2024-12-07 01:15:15 +0100

    About3 page added

--- src/components/RenderMarkdown.js
+++ src/components/RenderMarkdown.js
@@ -3,17 +3,17 @@ import ReactMarkdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 import remarkBreaks from "remark-breaks";
 
-function removeNewlines(input) {
-  return input.replace(/\n/g, "");
+function removeNewlinesAndWhitespace(input) {
+  return input.replace(/[\n\r\s]+/g, ""); // Matches all newlines, carriage returns, and whitespace.
 }
 
 const RenderMarkdown = ({ content }) => {
 
-  // const result = removeNewlines(content);
+  const result = removeNewlinesAndWhitespace(content);
   
   return (
     <ReactMarkdown
-      children={content}
+      children={result}
       rehypePlugins={[rehypeRaw]} // Allows raw HTML to be rendered
       // remarkPlugins={[remarkBreaks]} // Converts \n to <br />
     />
